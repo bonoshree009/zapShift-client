ccfe0ee3291f2b114f1b4c61ba0ca514ac240465
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2026-01-15 10:54:14 +0600

    firebase setup

---
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import React from 'react';
+import { auth } from '../../firebase/firebase.init';
+
+const AuthProvider = ({children}) => {
+    const registeruser =(email,password)=>{
+        return createUserWithEmailAndPassword (auth,email,password)
+    }
+    const signInUser =(email,password) =>{
+        return signInWithEmailAndPassword (auth,email,password)
+    }
+
+    const authinfo= {registeruser,signInUser}
+    return (
+        <AuthContext value={authinfo}> {children}
+            
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
